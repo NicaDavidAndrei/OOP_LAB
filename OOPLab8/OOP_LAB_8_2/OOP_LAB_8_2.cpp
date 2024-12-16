66a143e745e99ae5aa2d877220777a2e81f6a555
NicaDavidAndrei <nica.david.p7c@student.ucv.ro>
2024-12-16 22:48:36 +0200

    OOP_LAB_8_2.cpp

--- OOPLab8/OOP_LAB_8_2/OOP_LAB_8_2.cpp
+++ OOPLab8/OOP_LAB_8_2/OOP_LAB_8_2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 using namespace std;
 
 class Person
@@ -11,20 +10,18 @@ private:
     int age;
 
 public:
-    // Constructor
     Person(string name, int age)
     {
         this->name = name;
         this->age = age;
     }
 
-    // Getter for age
+    // Gatterer for age
     int getAge()
     {
         return age;
     }
 
-    // Display person info
     void display()
     {
         cout << "Name: " << name << ", Age: " << age << endl;
@@ -38,20 +35,18 @@ private:
     int yearsOld; // Age of the car
 
 public:
-    // Constructor
     Car(string model, int yearsOld)
     {
         this->model = model;
         this->yearsOld = yearsOld;
     }
 
-    // Getter for age (years old)
+    // Gatterer for age (vehicle age)
     int getAge()
     {
         return yearsOld;
     }
 
-    // Display car info
     void display()
     {
         cout << "Model: " << model << ", Years Old: " << yearsOld << endl;
@@ -62,7 +57,6 @@ template <typename T>
 class AverageCalculator
 {
 public:
-    // Method to compute the average age
     static double computeAverageAge(vector<T>& list)
     {
         if (list.empty())
@@ -81,15 +75,15 @@ int main()
 {
     // List of Persons
     vector<Person> persons = {
-        Person("Rodica", 30),
-        Person("Bobby", 25),
-        Person("Carlos", 35) };
+        Person("Elena", 30),
+        Person("Tudor", 25),
+        Person("Carlito", 35) };
 
     // List of Cars
     vector<Car> cars = {
         Car("Toyota", 5),
-        Car("Honda Civic", 3),
-        Car("Ford Focus", 10) };
+        Car("Tesla", 3),
+        Car("Dacia", 10) };
 
     // Compute and display average age for persons
     cout << "Persons in the list:" << endl;
